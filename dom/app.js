74775a10d5c571ccc69961ece62fccc17b73a331
Riashad Noor <riashad.noor@gmail.com>
2020-04-09 00:31:03 +0600

    Dom Practice upto Element Styling

--- dom/app.js
+++ dom/app.js
@@ -11,9 +11,13 @@ console.log(document.querySelector('.list-items'));
 console.log(document.querySelectorAll('.list-items'));
 
 
-// Get the first Item of the query
+/* Get the first Item of the query */
+// Won't show the live changes
 console.log(document.querySelector('ul li:first-of-type'));
-// Get the last Item of the query
+// Will show the live changes
+// console.log(document.getElementsByTagName('ul li:first-of-type'))
+
+/* Get the last Item of the query */
 console.log(document.querySelector('ul li:last-of-type'));
 
 
@@ -33,6 +37,7 @@ p1.style.background = "#ffAE25";
 
 
 p2.style.textTransform = 'UpperCase';
+p2.textContent = p2.textContent + '- I am Added using Js!';
 p3.style.textTransform = 'LowerCase';
 
 
@@ -63,3 +68,58 @@ const input2 = document.querySelector('#input-2');
 input3.value = input2.getAttribute('value');
 
 input1.setAttribute('value', 'This is set using Js Set Attribute');
+
+/*** Accessing the child nodes or Elements ***/
+
+console.log ('---- Accessing the Child Nodes or Elements ----');
+const ulList = document.querySelector('ul');
+console.log(ulList.children); // Displays all the children of Ul (without white-space)
+console.log(ulList.childNodes); //Displays all the child nodes of Ul (with white-space)
+console.log(ulList.childNodes[3]); //Displays Specific Child Node using array index calling
+console.log(ulList.firstChild); // Display the first child of child nodes - here will display #text as it's under white-space
+console.log(ulList.firstElementChild); //Display the first child of child nodes elements- here will display list item
+console.log(ulList.lastChild); // Display the last child of child nodes - here will display #text as it's under white-space
+console.log(ulList.lastElementChild); //Display the last child of child nodes elements- here will display list item
+
+/*** Accessing the Parent nodes or Elements ***/
+
+console.log ('---- Accessing the Parent Nodes or Elements ----');
+
+const lielm = document.querySelector('li');
+console.log(lielm.parentNode);
+console.log(lielm.parentElement);
+
+
+/*** Accessing the Previous Sibling nodes or Elements ***/
+
+console.log ('---- Accessing the Previous Sibling Nodes or Elements ----');
+
+const pSiblingElm = document.querySelector('ul');
+console.log(pSiblingElm.previousSibling); // Display the previous sibling nodes <of same level> - here will display #text as it's under white-space
+console.log(pSiblingElm.previousElementSibling); //Display the previous sibling nodes <of same level>- here will display list item
+
+/*** Accessing the Previous Sibling nodes or Elements ***/
+
+console.log ('---- Accessing the Next Sibling Nodes or Elements ----');
+
+const aSiblingElm = document.querySelector('ul');
+console.log(aSiblingElm.nextSibling); // Display the previous sibling nodes <of same level> - here will display #text as it's under white-space
+console.log(aSiblingElm.nextElementSibling); //Display the previous sibling nodes <of same level>- here will display list item
+
+
+
+/*** Using DOM Element for Styling ***/
+
+const section = document.querySelector('.section2');
+section.style.backgroundColor='green';
+
+const para = document.querySelector('.para-mod2');
+para.className = para.className + ' invisible';
+
+
+const btnCall = document.querySelector('#btnShowHide');
+
+
+btnCall.addEventListener('click', function(){
+    section.classList.toggle('invisible');
+}); 
